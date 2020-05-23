caed62331bca2c33e76621b0876eb0dee7e894f2
Amber-Pittman <amber@amberpittman.com>
2020-05-22 22:00:44 -0400

    Added production environment to knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,17 @@ const dbConnection = process.env.DATABASE_URL;
 module.exports = {
   development: {
     client: "sqlite3",
-    connection: { filename: "./database/users.db3" },
+    connection: { 
+      filename: "./database/users.db3" 
+    },
     useNullAsDefault: true,
     migrations: {
       directory: "./database/migrations",
       tableName: "dbmigrations"
     },
-    seeds: { directory: "./database/seeds" },
+    seeds: { 
+      directory: "./database/seeds" 
+    },
     pool: {
       afterCreate: (conn, done) => {
         conn.run("PRAGMA foreign_keys = ON", done);
@@ -22,7 +26,8 @@ module.exports = {
       },
       useNullAsDefault: true,
       migrations: {
-        directory: "./database/migrations"
+        directory: "./database/migrations",
+        tableName: "dbmigrations"
       },
       seeds: {
         directory: "./database/seeds"
@@ -32,6 +37,25 @@ module.exports = {
           conn.run("PRAGMA foreign_keys = ON", done);
         }
       }
-    }
+    },
+    production: {
+      client: "sqlite3",
+      connection: { 
+        filename: "./database/users.db3" 
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: "./database/migrations",
+        tableName: "dbmigrations"
+      },
+      seeds: { 
+        directory: "./database/seeds" 
+      },
+      pool: {
+        afterCreate: (conn, done) => {
+          conn.run("PRAGMA foreign_keys = ON", done);
+        }
+      }
+    },
   }
-};
+}
