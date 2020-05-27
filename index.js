8f7c20a65bd301965112dec3aff37b154a8262ad
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2020-05-26 19:45:36 -0600

    added protected route

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
+
 require('dotenv').config();
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 3000
-
-//if (!module.parent) {
-    server.listen(port, () => {
-        console.log(`Running at http://localhost:${port}`)
-    })
-//}
-
-module.exports = server
+server.listen(process.env.PORT, () => console.log(`\n** Running on port ${process.env.PORT} **\n`));
