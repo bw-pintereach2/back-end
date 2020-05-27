b8ebd0821fa9d95f3cbd5c053f3d3fd23a85e2c9
Amber-Pittman <amber@amberpittman.com>
2020-05-27 19:04:57 -0400

    Removed comments

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,8 +2,4 @@ const knex = require('knex')('production');
 
 const knexConfig = require('../knexfile.js');
 
-//const dbEnv = process.env.DB_ENV || 'development';
-
-//module.exports = knex(knexConfig[dbEnv]);
-
 module.exports = knex(knexConfig.development);
