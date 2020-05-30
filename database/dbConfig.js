5170e87b80657ccae5e163ebe677d4ceb091cafa
Amber-Pittman <amber@amberpittman.com>
2020-05-29 23:17:40 -0400

    Changed dbConfig back

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex')
 
 const knexConfig = require('../knexfile.js')
 
-const dbEnv = process.env.DB_ENV || 'production'
+const dbEnv = process.env.DB_ENV || 'development'
 
-module.exports = knex(knexConfig[dbEnv]);
+module.exports = knex(knexConfig[dbEnv])
