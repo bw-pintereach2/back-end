25536e66c190a8491467a93ea85b843566427cfe
Amber-Pittman <amber@amberpittman.com>
2020-05-29 23:40:11 -0400

    Updated register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,6 +8,9 @@ const router = express.Router();
 router.post("/register", async (req, res, next) => {
     try {
       const newUser = await add(req.body)
+      const hash = bcrypt.hashSync(user.password, 10)
+
+      user.password = hash
       
       res.status(201).json(newUser)
     } catch (err) {
@@ -15,58 +18,58 @@ router.post("/register", async (req, res, next) => {
     }
   })
   
-  router.post("/login", async (req, res, next) => {
-    try {
-      const { username, password } = req.body;
-      const user = await db("users")
-        .where({ username })
-        .first();
-      const passwordValid = await bcrypt.compare(password, user.password);
-  
-      if (user && passwordValid) {
-        const token = jwt.sign(
-          {
-            subject: user.id,
-            username: user.username
-          },
-          secrets.jwt,
-          {
-            expiresIn: "1d"
-          }
-        );
-  
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-          user_id: user.id,
-          token: token
-        });
-      } else {
-        res.status(401).json({
-          message: "Invalid Credentials"
-        });
-      }
-    } catch (err) {
-      next(err);
-    }
-  });
-  
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await db("users")
+      .where({ username })
+      .first();
+    const passwordValid = await bcrypt.compare(password, user.password);
 
-  // auth-model
-  async function add(user) {
-    
-      user.password = await bcrypt.hash(user.password, 10)
-    
-      const [id] = await db("users")
-        .insert(user)
-     
-      return findById(id)
+    if (user && passwordValid) {
+      const token = jwt.sign(
+        {
+          subject: user.id,
+          username: user.username
+        },
+        secrets.jwt,
+        {
+          expiresIn: "1d"
+        }
+      );
+
+      res.status(200).json({
+        message: `Welcome ${user.username}!`,
+        user_id: user.id,
+        token: token
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid Credentials"
+      });
     }
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+// auth-model
+async function add(user) {
+  
+    user.password = await bcrypt.hash(user.password, 10)
+  
+    const [id] = await db("users")
+      .insert(user)
     
-    function findById(id) {
-      return db("users")
-        .where({ id })
-        .first("id", "username")
-    }
+    return findById(id)
+  }
   
-  
-  module.exports = router;
+  function findById(id) {
+    return db("users")
+      .where({ id })
+      .first("id", "username")
+  }
+
+
+module.exports = router;
