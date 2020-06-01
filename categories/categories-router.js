3036de9322b7fb6ba099c3076556367ba0b9878d
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2020-05-31 18:32:43 -0600

    fixed get articles route

--- categories/categories-router.js
+++ categories/categories-router.js
@@ -52,7 +52,7 @@ router.get(`/:id`, (req, res) => {
 
   const id = req.params.id;
   db("articles")
-    .where({ id, user_id })
+    .where({ category_id, user_id })
     .then(articles => {
       res.json(articles);
     })
